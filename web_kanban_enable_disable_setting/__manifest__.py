77d24c752dc76a29d9fc6b7845e49fbf28d00446
Hichri Selman <s.hichri@tamkeentechnologie.sa>
2018-12-08 02:59:11 +0300

    [NEW] Migration odoo 1

--- web_kanban_enable_disable_setting/__manifest__.py
+++ web_kanban_enable_disable_setting/__manifest__.py
@@ -1,24 +1,4 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Odoo support
-#    Copyright (C) HICHRI Selman.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 {
     'name': "Web Kanban enable/disable settings",
 
@@ -32,7 +12,7 @@ to enable or disable the setting displayed in the header view on hover on the ti
     'author': "Hichri Selman",
     'category': 'Extra Tools',
     'version': '0.1',
-    'depends': ['base','web_kanban'],
+    'depends': ['base','web'],
     'data': [
         'views/assets.xml'
     ],
--- web_kanban_enable_disable_setting/static/src/js/kanban_attribute.js
+++ web_kanban_enable_disable_setting/static/src/js/kanban_attribute.js
@@ -1,32 +1,29 @@
 odoo.define('web_kanban_enable_disable_setting', function (require) {
     "use strict";
 
-    var quick_create = require('web_kanban.quick_create');
-    var KanbanColumn = require('web_kanban.Column');
-    var KanbanView = require('web_kanban.KanbanView');
+    var KanbanColumn = require('web.KanbanColumn');
+    var KanbanView = require('web.KanbanView');
 
     KanbanColumn.include({
         init: function(parent, group_data, options, record_options) {
             this._super.apply(this, arguments);
             this.enable_setting = true ;
-            if (options.enable_setting != 'undefined'){
-                this.enable_setting = options.enable_setting;
+            if (record_options.enable_setting != 'undefined'){
+                this.enable_setting = record_options.enable_setting;
             }
         },
     });
 
     KanbanView.include({
-        get_column_options: function () {
-            return {
-                editable: this.is_action_enabled('group_edit'),
-                deletable: this.is_action_enabled('group_delete'),
-                enable_setting: this.is_action_enabled('enable_setting'),
-                has_active_field: this.has_active_field(),
-                grouped_by_m2o: this.grouped_by_m2o,
-                relation: this.relation,
-                qweb: this.qweb,
-                fields: this.fields_view.fields,
-                quick_create: this._is_quick_create_enabled(),
+        init: function (viewInfo, params) {
+            var arch = viewInfo.arch;
+            this._super.apply(this, arguments);
+            var activeActions = this.controllerParams.activeActions;
+            this.rendererParams.record_options = {
+                editable: activeActions.edit,
+                deletable: activeActions.delete,
+                read_only_mode: params.readOnlyMode,
+                enable_setting:  arch.attrs.enable_setting,
             };
         },
     })
